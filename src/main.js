fd07704b94c22ccd3ed56548a57a989d70933f7c
jinqingsong <1211050148@qq.com>
2021-05-28 15:07:00 +0800

    点位抓拍统计详情

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import { HashRouter, Route, withRouter } from "react-router-dom";
 
+//首页
 import Homer from "./pages/home/index";
+//门禁点位统计详情
 import Details from "./pages/details/index";
+//点位抓拍统计详情
+import Facedetail from "./pages/facedetail/index";
 
 export default class Main extends Component {
   render() {
@@ -11,6 +15,7 @@ export default class Main extends Component {
         {/* <Route path="/login" exact component={Login}  onEnter={()=>{document.title='平台登录'}}></Route> */}
         <Route path="/" exact component={Homer}></Route>
         <Route path="/details" exact component={Details}></Route>
+        <Route path="/facedetail" exact component={Facedetail}></Route>
       </HashRouter>
     );
   }
